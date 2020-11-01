be1f7e2559a2446830f4e2565fbe5871aa93ce6d
Kapatsin Ilya <70647559+ilya3113@users.noreply.github.com>
2020-11-01 16:22:04 +0300

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,53 @@
+package Pr_9;
+
+
+public class Account {
+    private String INN;
+    private String name;
+
+    public Account(String INN, String name) {
+        this.INN = INN;
+        this.name = name;
+    }
+
+    public String get_INN() {
+        return INN;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void test_INN(String inn) throws Verification_INN {
+        if (inn.length() != 10)
+            throw new Verification_INN(this);
+    }
+
+    public static void main(String[] args) {
+        Account client = new Account("11223344556677889900", "Горшунов В.Е.");
+        try {
+            client.test_INN(client.get_INN());
+            System.out.println("ИНН клиента " + client.getName() + " действителен.");
+        } catch (Verification_INN e) {
+            System.out.println(e.getMessage());
+            ;
+        }
+
+        Account client1 = new Account("1234567890", "Петров В.Е.");
+        try {
+            client1.test_INN(client1.get_INN());
+            System.out.println("ИНН клиента " + client1.getName() + " действителен.");
+        } catch (Verification_INN e) {
+            System.out.println(e.getMessage());
+            ;
+        }
+
+        Account client2 = new Account("5641", "Иванов Е.М.");
+        try {
+            client2.test_INN(client2.get_INN());
+            System.out.println("ИНН клиента " + client2.getName() + " действителен.");
+        } catch (Verification_INN e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
